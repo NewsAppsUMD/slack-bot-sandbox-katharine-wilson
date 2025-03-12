67efc567259ef2e993a3fdff2bc0958c1387f864
Katharine Wilson <152829771+katharine-wilson@users.noreply.github.com>
2025-03-12 19:38:51 +0000

    working congress bot

--- congress.py
+++ congress.py
@@ -6,11 +6,9 @@ from slack import WebClient
 from slack.errors import SlackApiError
 
 slack_token = os.environ.get('SLACK_API_TOKEN')
-
-client = WebClient(token=slack_token)
-
 congress_api_key = "aKfCNxb80QWUylo4eaneyGasY8J4LREvxqkoRYy4"
 
+client = WebClient(token=slack_token)
 url = f"https://api.congress.gov/v3/committee-report/119/hrpt?api_key={congress_api_key}&format=json"
 
 r = requests.get(url)
@@ -28,4 +26,17 @@ formatted_date = display_date.strftime('%B %-d, %Y at %-I:%M%p')
 
 sentence = f"On {formatted_date}, the {first_result['chamber']} published {first_result['citation']}, which is available at {dispay_url}"
 
-msg = "sentence"
+msg = sentence
+
+try:
+    response = client.chat_postMessage(
+        channel="slack-bots",
+        text=msg,
+        unfurl_links=True, 
+        unfurl_media=True
+    )
+    print("success!")
+except SlackApiError as e:
+    assert e.response["ok"] is False
+    assert e.response["error"]
+    print(f"Got an error: {e.response['error']}")
